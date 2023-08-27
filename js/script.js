c4ae53fb4ed02593cf054d6c4331a26600a80bd6
gabrielSrufino <gabrielsr1206@outlook.com>
2023-08-27 14:23:57 -0300

    finalizao javasprit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-const changeThemeBtn = document.querySelector("#change-theme")
-changeThemeBtn.addEventListener("change", function(){
+const changeThemeBtn = document.querySelector("#change-theme");
 
-document.body.classList.toggle("dark")
+// Toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
 
+// Load light or dark mode
+function loadTheme() {
+  const darkMode = localStorage.getItem("dark");
+
+  if (darkMode) {
+    toggleDarkMode();
+  }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+  toggleDarkMode();
+
+  // Save or remove dark mode from localStorage
+  localStorage.removeItem("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  }
 });
